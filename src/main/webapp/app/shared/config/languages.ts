55810aec264fddd82fdc509b4f98f5bba45580b8
Clodoaldo da Fonseca <clodoaldofonseca92@gmail.com>
2025-04-08 22:03:36 -0300

    Initial version of myBudget generated by generator-jhipster@8.10.0

---
+++ src/main/webapp/app/shared/config/languages.ts
@@ -0,0 +1,7 @@
+const languages = () => ({
+  'pt-br': { name: 'Português (Brasil)' },
+  es: { name: 'Español' },
+  // jhipster-needle-i18n-language-key-pipe - JHipster will add/remove languages in this object
+});
+
+export default languages;
